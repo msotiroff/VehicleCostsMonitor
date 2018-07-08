f2f0856f7e99f2483eb07c2dd2d3348c86f5c12c
Mihail Sotirov <mihail.sotiroff@gmail.com>
2018-07-08 15:09:59 +0300

    Privacy policy updated!

--- VehicleCostsMonitor/VehicleCostsMonitor.Web/Startup.cs
+++ VehicleCostsMonitor/VehicleCostsMonitor.Web/Startup.cs
@@ -15,21 +15,20 @@ namespace VehicleCostsMonitor.Web
         }
 
         public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
+        
         public void ConfigureServices(IServiceCollection services)
         {
             services.Configure<CookiePolicyOptions>(options =>
             {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
-            
+
+            services.AddRouting(options => options.LowercaseUrls = true);
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
