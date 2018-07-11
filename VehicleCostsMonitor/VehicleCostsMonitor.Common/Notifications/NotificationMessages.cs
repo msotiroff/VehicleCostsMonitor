001aa857ac166c374e7c1716d4aa9ff54a067174
Mihail Sotirov <mihail.sotiroff@gmail.com>
2018-07-11 21:10:05 +0300

    Common classes for notification feature added!

---
+++ VehicleCostsMonitor/VehicleCostsMonitor.Common/Notifications/NotificationMessages.cs
@@ -0,0 +1,9 @@
+﻿namespace VehicleCostsMonitor.Common.Notifications
+{
+    public class NotificationMessages
+    {
+        public const string InvalidOperation = "Invalid operation!";
+
+        public const string ManufacturerDoesNotExist = "Manufacturer with id {0} does not exist!";
+    }
+}
