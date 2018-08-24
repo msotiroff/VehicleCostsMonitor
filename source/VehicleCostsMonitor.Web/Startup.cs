f1c8ad7a8c0143f70ad1c70a30317e71e0bfc77e
Mihail Sotirov <mihail.sotiroff@gmail.com>
2018-08-24 21:45:25 +0300

    DownloadMe.zip added!

--- source/VehicleCostsMonitor.Web/Startup.cs
+++ source/VehicleCostsMonitor.Web/Startup.cs
@@ -5,13 +5,11 @@
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
     using Microsoft.AspNetCore.Http;
-    using Microsoft.AspNetCore.Http.Features;
     using Microsoft.AspNetCore.Identity;
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.EntityFrameworkCore;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
-    using System.Linq;
     using System.Net.Http;
     using VehicleCostsMonitor.Data;
     using VehicleCostsMonitor.Models;
