c8d0ea3caed4960a87569ec316eb241ce6196ef2
Mihail Sotirov <mihail.sotiroff@gmail.com>
2018-09-02 18:27:54 +0300

    Some ignored files added to remote!

---
+++ source/VehicleCostsMonitor.Web/Areas/Admin/Models/Log/UserActivityLogListViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace VehicleCostsMonitor.Web.Areas.Admin.Models.Log
+{
+    using Infrastructure.Collections;
+    using Microsoft.AspNetCore.Mvc.Rendering;
+    using Services.Models.Log;
+    using System.Collections.Generic;
+
+    public class UserActivityLogListViewModel
+    {
+        public string SearchTerm { get; set; }
+
+        public IEnumerable<SelectListItem> AllSearchCriterias { get; set; }
+
+        public string SearchCriteria  { get; set; }
+
+        public PaginatedList<UserActivityLogConciseServiceModel> Logs { get; set; }
+    }
+}
